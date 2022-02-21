977ce66cb83fe9816776aa4902871b4a5b9432fe
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-21 10:30:17 +0530

    Initial Commit

---
+++ CombineWorks/CombineWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineWorksApp.swift
+//  CombineWorks
+//
+//  Created by shehan karunarathna on 2022-02-21.
+//
+
+import SwiftUI
+
+@main
+struct CombineWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
