16eabce921c43842ef5e303e1e5e850b7a26f776
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-21 10:50:22 +0530

    add a new publisher using @publisher modifier

--- CombineWorks/ContentView.swift
+++ CombineWorks/ContentView.swift
@@ -6,11 +6,66 @@
 //
 
 import SwiftUI
+import Combine
+
+class CombineDataService{
+    @Published var defaultPublisher: String = "First item"
+    let currentValuePublisher =  CurrentValueSubject<String,Never>("First Item")
+    init(){
+        publishFakeData()
+    }
+    private func publishFakeData(){
+        let items  = ["one", "two","three"]
+        for i in items.indices{
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.defaultPublisher = items[i]
+                
+            }
+        }
+       
+    }
+}
+
+class CombineViewModel:ObservableObject{
+    var cancelables = Set<AnyCancellable>()
+    @Published var data:[String] = []
+    let dataservice = CombineDataService()
+    init(){
+        addSubascribers()
+    }
+    
+    private func addSubascribers(){
+        dataservice.$defaultPublisher
+            .sink { completion in
+                switch completion{
+                case .finished:
+                    break
+                case .failure(let error):
+                    print("ERROR:\(error.localizedDescription)")
+                    break
+                    
+                }
+            } receiveValue: { [weak self]returnedVal in
+                self?.data.append(returnedVal)
+            }
+            .store(in: &cancelables)
+
+    }
+}
 
 struct ContentView: View {
+    @StateObject private var vm = CombineViewModel()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView{
+            VStack{
+                ForEach(vm.data, id:\.self){
+                    item in
+                    Text(item)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+            }
+        }
     }
 }
 
